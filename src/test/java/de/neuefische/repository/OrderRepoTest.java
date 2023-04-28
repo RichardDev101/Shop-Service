52c38b31d55dbc727e75d7dc479b3d17100ba84d
Oliver Hofrichter <oli.bremen@gmail.com>
2023-04-28 11:21:20 +0200

    added test classes for repos

---
+++ src/test/java/de/neuefische/repository/OrderRepoTest.java
@@ -0,0 +1,7 @@
+package de.neuefische.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderRepoTest {
+
+}
---
+++ src/test/java/de/neuefische/repository/ProductRepoTest.java
@@ -0,0 +1,14 @@
+package de.neuefische.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepoTest {
+
+    @Test
+    void test() {
+
+    }
+
+}
