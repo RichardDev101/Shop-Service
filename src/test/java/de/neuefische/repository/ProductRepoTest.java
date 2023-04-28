335f484be183bfba9f1e08b3f3f856935f5b4135
Oliver Hofrichter <oli.bremen@gmail.com>
2023-04-28 13:41:26 +0200

    added test cases in ProductRepoTest class

--- src/test/java/de/neuefische/repository/ProductRepoTest.java
+++ src/test/java/de/neuefische/repository/ProductRepoTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductRepoTest {
 
     @Test
-    void testGet() {
+    void testGet_positive_case_one_product() {
         // given
         Product expected = new Product("p1", "Toaster");
         ProductRepo productRepo = new ProductRepo();
@@ -24,7 +24,40 @@ class ProductRepoTest {
 
         // then
         assertEquals(expected, actual);
+    }
 
+    @Test
+    void testGet_positive_case_several_products() {
+        // given
+        Product product1 = new Product("p1", "Toaster");
+        Product expected = new Product("p2", "Waschmaschine");
+        ProductRepo productRepo = new ProductRepo();
+        Map<String, Product> productMap = new HashMap<>();
+        productMap.put(expected.getId(), expected);
+        productRepo.setProducts(productMap);
+
+        // when
+        Product actual = productRepo.get("p2");
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGet_negative_case() {
+        // given
+        Product product1 = new Product("p1", "Toaster");
+        Product product2 = new Product("p2", "Waschmaschine");
+        ProductRepo productRepo = new ProductRepo();
+        Map<String, Product> productMap = new HashMap<>();
+        productMap.put(product2.getId(), product2);
+        productRepo.setProducts(productMap);
+
+        // when
+        Product actual = productRepo.get("p3");
+
+        // then
+        assertNull(actual);
     }
 
 }
